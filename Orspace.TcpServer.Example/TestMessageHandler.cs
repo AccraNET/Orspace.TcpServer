746b255131d8aba447c1fda37fe42d16d5eebc21
Cerezo Larbi <cerezo.larbi@outlook.com>
2022-08-22 21:01:41 +0000

    Updatad test application to stream back connection details about the client

--- Orspace.TcpServer.Example/TestMessageHandler.cs
+++ Orspace.TcpServer.Example/TestMessageHandler.cs
@@ -3,6 +3,8 @@ using Orspace.TcpServer.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Security;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,7 +31,17 @@ namespace Orspace.TcpServer.Example
             try
             {
                 NetworkStream stream = client.GetStream();
-                await stream.WriteAsync(Encoding.UTF8.GetBytes(test), 0, test.Length);
+                await stream.WriteAsync(Encoding.UTF8.GetBytes(test + "\n"), 0, test.Length + 1);
+
+                //Remote IP address
+                string remoteIP = (client.Client.RemoteEndPoint as IPEndPoint).Address.ToString();
+
+                //Remote Port
+                string remotePort = (client.Client.RemoteEndPoint as IPEndPoint).Port.ToString();
+
+                await stream.WriteAsync(Encoding.UTF8.GetBytes($"Remote IP      : {remoteIP}\n"));
+                await stream.WriteAsync(Encoding.UTF8.GetBytes($"Remote Port    : {remotePort}\n"));
+
                 await Task.Delay(2000);
             }
             catch (Exception ex)
